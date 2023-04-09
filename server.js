772bbef55a87969c07db5c2e793fa132d623f595
Alexandr Lebedinsky <mblandr@gmail.com>
2023-04-09 10:37:56 +0300

    sadfasdf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import pug from 'pug';
 const app = App();
 app
 	.get('/', (res, req) => res.end(pug.renderFile('views/index.pug')))
-	.listen(80, (socket) => {
+	.listen(3000, (socket) => {
 		if (socket) console.log('Server is running');
 		else console.error('Error starting');
 	});
